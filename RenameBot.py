43ac214bb4aea1dc70845c7fd05e165ff7e4be95
ZabeMath <alec@vc-celle.de>
2021-05-10 18:23:29 +0200

    Add die command

--- RenameBot.py
+++ RenameBot.py
@@ -20,6 +20,7 @@ class FreenodeBot(SASL, SSL, DisconnectOnError, Ghost, Bot):
     def __init__(self):
         self.channel = config.channel
         self.nickname = config.nick
+        self.owner = config.owner
 
         super().__init__(
             server_list=[(config.server, 6697)],
@@ -38,6 +39,54 @@ class FreenodeBot(SASL, SSL, DisconnectOnError, Ghost, Bot):
         elif event.arguments[0] == "PING" and len(event.arguments) > 1:
             c.ctcp_reply(nm_to_n(event.source), "PING " + event.arguments[1])
 
+    def on_pubmsg(self, c, event):
+        if not self.has_primary_nick():
+            return
+
+        nick = event.source.nick
+        a = event.arguments[0]
+        where = event.target
+        who = "<" + where + "/" + nick + "> "
+        if where == self.channel:
+            if a[0] == "@" or a.lower().startswith(
+                    self.nickname.lower() + ":"):
+                # Start of Anti-PiR hack
+                evilchars = (";", '"')
+                for evilchar in evilchars:
+                    if evilchar in a:
+                        self.msg(
+                            "Your command contains prohibited characters. Please repeat the command without them.",
+                            self.channel)
+                        return
+                # End of Anti-PiR hack
+                if a[0] == "@":
+                    command = a[1:]
+                else:
+                    command = re.sub("(?i)%s:" %
+                                     self.nickname.lower(), "", a).strip(" ")
+
+                if command.lower in ["die"]:
+                    self.do_command(event.source, command.strip())
+
+    def do_command(self, e, cmd, target=None):
+        nick = nm_to_n(e)
+        if not target:
+            target = self.channel
+
+        if cmd.lower() == "die":
+            if self.getcloak(e) != self.owner:
+                if not self.quiet:
+                    logger.warning('Unprivileged %s requested exit', self.getcloak(e))
+                    self.msg("You can't kill me; you're not my owner! :P")
+            else:
+                self.msg("Goodbye!")
+
+                # cause recent changes listener to stop on next event
+                bot2.should_exit = True
+
+                self.connection.part(self.channels)
+                self.disconnect()
+
     def msg(self, message, target=None):
         if not target:
             target = self.channel
@@ -45,6 +94,10 @@ class FreenodeBot(SASL, SSL, DisconnectOnError, Ghost, Bot):
             self.connection.privmsg(target, message)
         except:
             self.connection.privmsg(target, "The message is too long.")
+
+    def getcloak(self, doer):
+        if re.search("/", doer) and re.search("@", doer):
+            return doer.split("@")[1]
 
 
 class RecentChangesBot:
